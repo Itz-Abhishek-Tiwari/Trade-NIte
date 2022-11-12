ed40d5b5f64b7190bf17243dbd1e7275aa611d44
Abhishek Tiwari <69257109+Itz-Abhishek-Tiwari@users.noreply.github.com>
2022-11-12 11:55:51 +0530

    mobile view

--- script.js
+++ script.js
@@ -2,5 +2,35 @@ function CopyMenu() {
   var dptCategory = document.querySelector(".dpt-cat");
   var dptPlace = document.querySelector(".departments");
   dptPlace.innerHTML = dptCategory.innerHTML;
+
+  var mainNav = document.querySelector(".header-nav nav");
+  var navPlace = document.querySelector(".off-canvas nav");
+  navPlace.innerHTML = mainNav.innerHTML;
+
+  var topNav = document.querySelector(".header-top .wrapper");
+  var topPlace = document.querySelector(".off-canvas .thetop-nav");
+  topPlace.innerHTML = topNav.innerHTML;
 }
 CopyMenu();
+
+// Show mobile menu
+const menuButton = document.querySelector(".trigger"),
+  closeButton = document.querySelector(".t-close"),
+  addclass = document.querySelector(".site");
+menuButton.addEventListener("click", function () {
+  addclass.classList.toggle("showmenu");
+});
+closeButton.addEventListener("click", function () {
+  addclass.classList.remove("showmenu");
+});
+const submenu = document.querySelectorAll(".has-child .icon-small");
+submenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e) {
+  e.preventDefault();
+  submenu.forEach((item) =>
+    item != this ? item.closest(".has-child").classList.remove("expand") : null
+  );
+  if (this.closest(".has-child").classList != "expand")
+    this.closest(".has-child").classList.toggle("expand");
+}
